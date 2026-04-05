ba2300bb2350a27383b91173f7d74d1dfb894ca8
kote-e <mjosesanm@gmail.com>
2026-04-04 23:51:39 -0400

    actualizacion

--- Trabajos/trabajo1/carrera.py
+++ Trabajos/trabajo1/carrera.py
@@ -1,4 +1,4 @@
-#importaciones necesarias
+#importaciones
 import json
 from random import choice, randint, sample  
 from tortuga import Tortuga
@@ -15,15 +15,15 @@ with open("nombres_tortugas.json", "r") as f:
 tortugas = []
 todas = nombres_tortugas["tortugas"]
 seleccionados= sample(todas, 10)
-#print(tortugas)
 
+#print(tortugas)
 print(Style.BRIGHT + Fore.CYAN + f"¡Bienvenido a la carrera de tortugas!" + Style.RESET_ALL)
 dinero_total= int(input("¿Cuánto dinero tienes para apostar?: ")) # para que el usuario pueda apostar, y se le vaya restando o sumando dependiendo de si gana o pierde la apuesta.
+
 for t in seleccionados:
     tortuga = Tortuga()
-    tortuga.crear_tortuga(t["nombre"], t["frase"])
+    tortuga.crear_tortuga(t["nombre"], t["frase_ganador"], t["frase_top3"], t["frase_perdedor"])
     tortugas.append(tortuga)
-
 print(f"las tortugas que correran son:")
 for t in tortugas:
     print(Style.BRIGHT + Fore.MAGENTA + f"- "+ Style.RESET_ALL + f"{t.nombre}")
@@ -31,7 +31,7 @@ for t in tortugas:
 
 #sistema de apuesta
 elegida= input("¿En qué tortuga quieres apostar?: ")
-while elegida not in [t.nombre for t in tortugas]:
+while elegida.strip().lower() not in [t.nombre.lower() for t in tortugas]:
     print("Tortuga no válida. Por favor, elige una tortuga de la lista.")
     elegida = input("¿En qué tortuga quieres apostar?: ")
 
@@ -42,8 +42,6 @@ while apuesta > dinero_total:
 
 #carrera
 print(f"\n{Style.BRIGHT + Fore.CYAN}🏁 ¡Comienza la carrera! 🏁{Style.RESET_ALL}\n")
-
-
 alguien_gano = False
 while not alguien_gano:
     posiciones = []
@@ -63,8 +61,8 @@ while not alguien_gano:
     for i in posiciones:
         i.posicion_carrera= posiciones.index(i) + 1
     
+    #eventos aleatorios
     for t in posiciones:
-        #eventos aleatorios
         if randint(1, 100) <= 6:  # 6% de probabilidad de que ocurra un evento
             evento= choice(["aji", "alas", "burocracia", "catapulta", "hongo"])
             if evento == "aji":
@@ -82,7 +80,7 @@ while not alguien_gano:
                         break
             elif evento == "burocracia":
                 t.distancia_recorrida -= t.velocidad
-                print(Style.BRIGHT + Fore.YELLOW + f"¡{t.nombre} no avanza!" + Style.RESET_ALL)
+                print(Style.BRIGHT + Fore.YELLOW + f"¡{t.nombre} esta en Firma burocratica y no avanza!" + Style.RESET_ALL)
             elif evento == "catapulta":
                 t.avanza= False
                 t.si_no_abanza= 3
@@ -91,12 +89,13 @@ while not alguien_gano:
             elif evento == "hongo":
                 if randint(0,1)== 0:
                     t.distancia_recorrida += 40
-                    print(Style.BRIGHT + Fore.GREEN + f"¡{t.nombre} comio un hongo, otiene una apertura de conciencia lo que le permite encontrar un atajo  y avanza 40 metros extra!" + Style.RESET_ALL)
+                    print(Style.BRIGHT + Fore.GREEN + f"¡{t.nombre} comio un hongo y otiene una apertura de conciencia lo que le permite encontrar un atajo  y avanza 40 metros extra!" + Style.RESET_ALL)
                 else:
                     t.avanza= False
                     t.si_no_abanza= 4
-                    print(Style.BRIGHT + Fore.GREEN + f"¡{t.nombre} comio un hongo, pero no paso un buentiampo! Se desmaya y no avanza durante 4 turnos!" + Style.RESET_ALL)
-                
+                    print(Style.BRIGHT + Fore.GREEN + f"¡{t.nombre} comio un hongo, pero paso un bad time! Se desmaya y no avanza durante 4 turnos!" + Style.RESET_ALL)
+            else:
+                pass
 
     if posiciones[0].distancia_recorrida >= 1000:
         alguien_gano = True
@@ -106,24 +105,28 @@ while not alguien_gano:
 
 #ganaores
 #ganador= choice(tortugas) # solo para confirmar que el sistema de apuestas funciona :)
-print(f"¡La carrera ha terminado! El ganador es: {ganador.nombre}")
-print(Style.BRIGHT + Fore.YELLOW + f"{ganador.nombre}: "+ Style.RESET_ALL + f"{ganador.frase}" )
+
+
+print(f"\n¡La carrera ha terminado! El ganador es: {ganador.nombre}")
+print(Style.BRIGHT + Fore.YELLOW + f"{ganador.nombre}: "+ Style.RESET_ALL + f"{ganador.frase_ganador}" )
 
 #3 mejores
 print(f"\n {Style.BRIGHT + Fore.CYAN}🏆 ¡Resultados de la carrera! 🏆{Style.RESET_ALL}\n")
-for i in enumerate(posiciones[:-1]):
-    if i[0] < 3:
-        print(Style.BRIGHT + Fore.YELLOW + f"{i[0]+1}. "+ Style.RESET_ALL + f"{i[1].nombre} recorrio: {i[1].distancia_recorrida} metros")
+for i in enumerate(posiciones[:10]):
+    if i[0] == 0:
+        print(Style.BRIGHT + Fore.GREEN + f"{i[0]+1}. "+ Style.RESET_ALL + f"{i[1].nombre} recorrio: {i[1].distancia_recorrida} metros")
+    elif i[0] < 3:
+        print(Style.BRIGHT + Fore.YELLOW + f"{i[0]+1}. "+ Style.RESET_ALL + f"{i[1].nombre} recorrio: {i[1].distancia_recorrida} metros: {i[1].frase_top3}")
     else:
-        print(f"{i[0]+1}. {i[1].nombre} recorrio: {i[1].distancia_recorrida} metros")
+        print(f"{i[0]+1}. {i[1].nombre} recorrio: {i[1].distancia_recorrida} metros: {i[1].frase_perdedor}")
 
 
 
 if ganador.nombre == elegida:
     dinero_total += apuesta
-    print(Style.BRIGHT + Fore.GREEN+ f"¡Felicidades! {elegida} ha ganado. Tu nueva cantidad de dinero es: {dinero_total}" + Style.RESET_ALL)
+    print(Style.BRIGHT + Fore.GREEN+ f"¡Felicidades! {elegida.strip()} ha ganado. Tu nueva cantidad de dinero es: {dinero_total}" + Style.RESET_ALL)
     
 else:
     dinero_total -= apuesta
-    print(Style.BRIGHT + Fore.RED + f"Lo siento, {elegida} no ha ganado. Tu nueva cantidad de dinero es: {dinero_total}" + Style.RESET_ALL)
+    print(Style.BRIGHT + Fore.RED + f"Lo siento, {elegida.strip()} no ha ganado. Tu nueva cantidad de dinero es: {dinero_total}" + Style.RESET_ALL)
     
--- Trabajos/trabajo1/tortuga.py
+++ Trabajos/trabajo1/tortuga.py
@@ -6,11 +6,15 @@ class Tortuga:
         self.velocidad = 0
         self.posicion_carrera = 0
         self.distancia_recorrida = 0
-        self.frase = ""
+        self.frase_ganador = ""
+        self.frase_top3 = ""
+        self.frase_perdedor = ""
         self.avanza= True
         self.si_no_abanza= 0
 
-    def crear_tortuga(self, nombre, frase):
+    def crear_tortuga(self, nombre, frase1, frase2, frase3):
         self.nombre = nombre
-        self.frase = frase
+        self.frase_ganador = frase1
+        self.frase_top3 = frase2
+        self.frase_perdedor = frase3
 
